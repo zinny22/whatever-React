d0f6e88d8be65dad2ff2a14d9af202c218113950
zinny22 <hziny722@naver.com>
2025-04-02 23:16:36 +0900

    refactor: useState 상태 구조를 let → const 로 리팩토링

--- src/lib/useState.js
+++ src/lib/useState.js
@@ -1,34 +1,37 @@
 import { main } from "../main";
 
 /**
+ * 헷갈렸던 부분 1 const로 선언한 객체에 직접 할당은 불가능 한줄..
+ * but const로 선언한 객체는 참조값 변경이 불가능한 거지, 객체 안의 속성 값은 바꿀 수 있음
+ */
+const state = { states: [], currentIndex: 0 };
+
+/**
  * 전역 상태 저장소 역할
  * states: 모든 컴포넌트의 상태값 저장
  * currentIndex: 현재 실행중인 useState가 몇번째인지
  */
-let states = [];
-export let currentIndex = 0;
-
 export default function useState(initialValue) {
-  const index = currentIndex;
+  const index = state.currentIndex;
 
   // 값이 저장되어 있지 않으면, 초기값
   // useState는 호출 순서대로 상태를 저장함
-  if (states[index] === undefined) {
-    states[index] = initialValue;
+  if (state.states[index] === undefined) {
+    state.states[index] = initialValue;
   }
 
   const setState = (newValue) => {
-    states[index] = newValue;
+    state.states[index] = newValue;
     // 상태 변경 후 앱 전체 렌더링 진행
     main();
   };
 
   //  다음 useState 인덱스 +1
-  currentIndex++;
-  return [states[index], setState];
+  state.currentIndex++;
+  return [state.states[index], setState];
 }
 
 // 렌더링 시작 전 인덱스를 리셋
 export function resetIndex() {
-  currentIndex = 0;
+  state.currentIndex = 0;
 }
