223ea768a432cc28dd0a5abe00d46bdc561204fd
zinny22 <hziny722@naver.com>
2025-04-02 23:30:01 +0900

    refactor: createElement 내 children 처리 조건 수정

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,21 +1,28 @@
 /**
- * createElement 함수
+ * createElement 함수 -> vdom 생성해 주는 함수
  * @param {string} type - DOM 태그 이름
  * @param {Object} props - props
  * @param {...*} children - 자식 요소
  * @returns {Object} - VNode
  */
 function createElement(type, props, ...children) {
+  const filteredChildren = children.filter(
+    (child) => child !== undefined && child !== null && child !== false
+  );
+
+  // props와 children이 모두 없으면 props 생략
+  if (!props && filteredChildren.length === 0) {
+    return { type };
+  }
+
   return {
+    // 타입에 대한 처리는 렌더함수에서 진행
     type,
     props: {
       ...(props || {}),
       children:
-        children.length === 0
-          ? undefined
-          : children.length === 1
-          ? children[0]
-          : children,
+        // 1개 일때는 그 자체로, 2개 이상부터 배열의 형식으로 들어가야 함
+        filteredChildren.length === 1 ? filteredChildren[0] : filteredChildren,
     },
   };
 }
