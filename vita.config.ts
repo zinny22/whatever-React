de0e671f6e540bd339da28b713a743b69c37c06a
zinny22 <hziny722@naver.com>
2025-04-16 15:42:47 +0900

    chore: vite 설정 파일 추가

---
+++ vita.config.ts
@@ -0,0 +1,11 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  esbuild: {
+    jsxFactory: "createElement",
+    jsxFragment: "Fragment",
+  },
+});
