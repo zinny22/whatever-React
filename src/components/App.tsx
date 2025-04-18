9a2d16168d2d5625402fa20173c24073393f3651
zinny22 <hziny722@naver.com>
2025-04-19 03:17:07 +0900

    fix: 간단한 todoList 구현

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,18 +3,33 @@ import useState from "../hooks/useState";
 import Title from "./Title";
 
 function App() {
-  let inputEl: HTMLInputElement | null = null;
+  let value = "";
   const [list, setList] = useState(["일번"]);
-  const onChangeRef = (e: HTMLInputElement) => (inputEl = e);
+
+  const onClickAdd = () => {
+    if (value === "") {
+      alert("할 일을 입력해주세요");
+      return;
+    }
+
+    setList([...list, value]);
+    value = "";
+  };
 
   return (
     <div className="list">
       <Title />
 
-      <input type="text" ref={onChangeRef} placeholder="할 일을 입력하세요" />
-      <button onClick={() => setList([...list, inputEl?.value || ""])}>
-        추가
-      </button>
+      <input
+        type="text"
+        value={value}
+        onChange={(e: any) => {
+          const newValue = e.target.value;
+          value = newValue;
+        }}
+        placeholder="할 일을 입력하세요"
+      />
+      <button onClick={onClickAdd}>추가</button>
 
       <ul>
         {list.map((item, index) => (
